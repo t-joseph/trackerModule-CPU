0997e41d0a9838fdb97967b750f4589881aea64a
tomjo <tj-171874@hs-weingarten.de>
2018-05-22 10:11:33 +0200

    updating header files

--- bpi.h
+++ bpi.h
@@ -11,33 +11,35 @@ using namespace std;
 
 
 // Initiator module generating generic payload transactions
-
 SC_MODULE (bpi)
 {
 
   public:
-  //input ports
+  //input port
   sc_port<sc_fifo_in_if<char> >     bpi_i ;      //
+  //output port
   sc_port<sc_fifo_out_if<char> >    bpi_o ;      //
 
   // TLM-2 socket, defaults to 32-bits wide, base protocol
   tlm_utils::simple_initiator_socket<bpi> socket;
 
+
   SC_HAS_PROCESS(bpi);
   bpi (sc_module_name nm);
 
-  // Internal data buffer used by initiator with generic payload
-  enum { addrGpsFrame = 10, addrGsmFrame = 11, frameSize = 100};
+  // Enum datatype defining constants
+  enum {addrGpsFrame = 10, addrGsmFrame = 11, frameSize = 100};
 
+  //local variable
   char data[frameSize];
 
 
   private:
 
-//signals
+  //signals
   sc_fifo<char> bpiRd_f;
 
-
+  //events
   sc_event bpiCpuRd_e1;
   sc_event thread_process_e2;
   sc_event thread_process_e3;
--- controller.h
+++ controller.h
@@ -19,45 +19,42 @@ SC_MODULE (controller) {
 
   public:
   //input ports
-  sc_port<sc_fifo_in_if<char> >     gps_i ;      //
-  sc_port<sc_fifo_in_if<char> >     gsm_i ;      //
+  sc_port<sc_fifo_in_if<char> >     gps_i ;
+  sc_port<sc_fifo_in_if<char> >     gsm_i ;
 
   //output ports
-  sc_port<sc_fifo_out_if<char> >    gsm_o ;     //
+  sc_port<sc_fifo_out_if<char> >    gsm_o ;
 
-  sc_port<sc_fifo_in_if<char> >     cpu_i ;      //
-  sc_port<sc_fifo_out_if<char> >    cpu_o ;      //
-
-
+  //Bus Periperal Interface Ports(part of CPU)
+  sc_port<sc_fifo_in_if<char> >     cpu_i ;
+  sc_port<sc_fifo_out_if<char> >    cpu_o ;
 
   //debug ports
-/*
-  sc_in<sc_uint<DBG_WIDTH> >        dbg_i ;      //
-  sc_in<bool> 			   dbg_en;  //
-  sc_out<sc_uint<DBG_WIDTH> >       dbg_o ;     //
-*/
+  sc_port<sc_fifo_in_if<char> >   dbg_i ;
+  sc_in<bool> 			              dbgEn_i;
+  sc_port<sc_fifo_out_if<char> >  dbg_o ;
+  sc_in<bool> 			              dbgCapture_i;
+  sc_in<bool> 			              dbgUpdate_i;
+  sc_in<bool> 			              dbgScan_i;
 
+  // Local Variables
+  string gpsFrame;
+  string gsmFrame;
+  string dataFrame;
 
-// Local Variables
-string gpsFrame;
-string gsmFrame;
-string dataFrame;
+  // Constructor prototype
+  SC_HAS_PROCESS(controller);
+  controller (sc_module_name nm);
 
-// Constructor prototype
-//SC_CTOR(controller);
+  private:
 
-SC_HAS_PROCESS(controller);
-controller (sc_module_name nm);
-
-private:
-
-//signals
+  //signals
   sc_fifo<char> gpsRd_f;
   sc_fifo<char> gsmRd_f;
   sc_fifo<char> cpuRd_f;
 
 
-// events
+  // events
   sc_event sleep_e1;
   sc_event user_request_e1;
   sc_event gpsframe_read_e2;
@@ -66,23 +63,23 @@ private:
   sc_event dataframe_gen_e5;
   sc_event dataframe2gsm_write_e6;
 
-//sleep thread
-void sleep();
+  //sleep thread
+  void sleep();
 
-//Read GPS frame from the FIFO
-void gpsToCpuRdThread(void);
+  //Read GPS frame from the FIFO
+  void gpsToCpuRdThread(void);
 
-//Request Power data to GSM
-void cpuToGsmWrThread(void);
+  //Request Power data to GSM
+  void cpuToGsmWrThread(void);
 
-//Read GSM frame from the FIFO
-void gsmToCpuRdThread(void);
+  //Read GSM frame from the FIFO
+  void gsmToCpuRdThread(void);
 
-// computation thread
-void cpuCompThread(void);
+  // computation thread
+  void cpuCompThread(void);
 
-// Write Data frame to Output FIFO(GSM)
-void cpuToGsmDataFrameWrThread(void);
+  // Write Data frame to Output FIFO(GSM)
+  void cpuToGsmDataFrameWrThread(void);
 };
 
 #endif
